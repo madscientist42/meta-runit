ff4145d398181e507210d9582087c3ffa2debce8
Frank Earl <frank@earlconsult.com>
2019-06-06 16:08:38 -0500

    Corrected errors in the core-services item for dtbo
    One typo correction and a cleanup.

--- recipes-core/runit-base-services/files/core-services/03-dtbo-load.sh
+++ recipes-core/runit-base-services/files/core-services/03-dtbo-load.sh
@@ -10,26 +10,28 @@ CONFIGFS_DIR="/sys/kernel/config/device-tree/overlays"
 #  an alternate, "official" one that may be patched in or is 
 #  provided with some of the latest kernels that differs slightly 
 #  in use.  We'll need to fix this script when we encounter it.)
-lsmod | grep -q dtbocfgif [ $? -eq 0 ] ; then
-
+lsmod | grep -q dtbocfg
+if [ $? -eq 0 ] ; then
     # Double check to make sure we have the dtbocfg hook fully
     # in place and we have a directory from to which to install
     # DTBOs from...
-    if [ -e $CONFIGFS_DIR -a -e /etc/dtbo ] ; then         
-        msg "Installing Device Tree Overlays to the system"
+    if [ -e $CONFIGFS_DIR -a -e /etc/dtbo ] ; then
+        if [ "$(ls -A /etc/dtbo)" ] ; then 
+            msg "Installing Device Tree Overlays to the system"
 
-        # Switch to the configfs space we need to drive...
-        cd $CONFIGFS_DIR
+            # Switch to the configfs space we need to drive...
+            cd $CONFIGFS_DIR
 
-        # For now, presume that /etc/dtbo holds the overlay configs and
-        # we're going to push each and enable, in turn, from that directory.
-        for dtbo in /etc/dtbo/* ; do
-            dtbo_name=`basename $dtbo`
-            mkdir $dtbo_name
-            cp $dtbo $dtbo_name/dtbo
-            echo 1 > $dtbo_name/status
-        done
+            # For now, presume that /etc/dtbo holds the overlay configs and
+            # we're going to push each and enable, in turn, from that directory.
+            for dtbo in /etc/dtbo/* ; do
+                dtbo_name=`basename $dtbo`
+                mkdir $dtbo_name
+                cp $dtbo $dtbo_name/dtbo
+                echo 1 > $dtbo_name/status
+            done
 
-        msg_done "Loaded overlays"
+            msg_done "Loaded overlays"
+        fi 
     fi     
 fi
