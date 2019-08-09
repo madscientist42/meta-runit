3f1419f46762eb0267638e74efb89af8cf340516
Frank Earl <frank@earlconsult.com>
2019-08-08 20:49:52 -0500

    Added missing mount to /run tmpfs entry
    Mostly works right without it...MOSTLY.  So we're going to mount
    this before 04-filesystems.sh which is sound and safe with that
    core-services init.

--- recipes-core/runit-base-services/files/core-services/02-udev.sh
+++ recipes-core/runit-base-services/files/core-services/02-udev.sh
@@ -5,6 +5,11 @@
 [ -n "$VIRTUALIZATION" ] && return 0
 
 msg "Starting udev and waiting for devices to settle..."
+
+# Presume the fstab has a /run tmpfs mount for us to work with- we're
+# early enough we want to just do this...
+mount /run
+
 mkdir -p /run/udev
 udevd --daemon
 udevadm trigger --action=add --type=subsystems
