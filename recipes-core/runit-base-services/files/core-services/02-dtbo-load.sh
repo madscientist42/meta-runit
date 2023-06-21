64667650de67d27221fa989181c942507fa78ce9
Frank Earl <frank@earlconsult.com>
2023-06-20 22:48:24 -0500

    Clean up dtbo load support. - It needed to account for older   kernels where we provide   DTBO load/config support via   the dtbocfg kernel module and   handle the 6.x kernels which   have the functionality built in.

--- recipes-core/runit-base-services/files/core-services/02-dtbo-load.sh
+++ recipes-core/runit-base-services/files/core-services/02-dtbo-load.sh
@@ -2,37 +2,41 @@
 
 . /etc/runit/functions
 
+# NOTE: This is currently the "correct" path for things and the
+# behaviors in this directory is 1-for-1 the same whether you're
+# talking the Xilinx patches, 6.x behaviors, or dtbocfg out of tree
+# module.
 CONFIGFS_DIR="/sys/kernel/config/device-tree/overlays"
 
-# Check to see if we've got DTBO load support lit in the kernel...
-# (FIXME - Presume that this is driven by dtbocfg as it's the 
-#  most readily available out-of-tree without patches.  There's
-#  an alternate, "official" one that may be patched in or is 
-#  provided with some of the latest kernels that differs slightly 
-#  in use.  We'll need to fix this script when we encounter it.)
-modprobe dtbocfg
-lsmod | grep -q dtbocfg
-if [ $? -eq 0 ] ; then
-    # Double check to make sure we have the dtbocfg hook fully
-    # in place and we have a directory from to which to install
-    # DTBOs from...
-    if [ -e $CONFIGFS_DIR -a -e /etc/dtbo ] ; then
-        if [ "$(ls -A /etc/dtbo)" ] ; then 
-            msg "Installing Device Tree Overlays to the system"
+# Check to see if we have CONFIGS_DIR present.  If not, we're going
+# to check and see if we can/need to load dtbocfg since we DO offer
+# this support for people if they're not in one of the other places.
+if [ ! -e $CONFIGFS_DIR ] ; then
+    find /lib/modules | grep -q dtbocfg
+    if [ $? -eq 0 ]; then
+        # Attempt modprobing the module...
+        modprobe dtbocfg
+    fi
+fi
 
-            # Switch to the configfs space we need to drive...
-            cd $CONFIGFS_DIR
+# Now, check to see if we've got the CONFIGS_DIR and /etc/dtbos present...
+# If so, proceed to attempt to load DTBOs into the DT data store.
+if [ -e $CONFIGFS_DIR -a -e /etc/dtbo ] ; then
+    if [ "$(ls -A /etc/dtbo)" ] ; then
+        msg "Installing Device Tree Overlays to the system"
 
-            # For now, presume that /etc/dtbo holds the overlay configs and
-            # we're going to push each and enable, in turn, from that directory.
-            for dtbo in /etc/dtbo/* ; do
-                dtbo_name=`basename $dtbo`
-                mkdir $dtbo_name
-                cp $dtbo $dtbo_name/dtbo
-                echo 1 > $dtbo_name/status
-            done
+        # Switch to the configfs space we need to drive...
+        cd $CONFIGFS_DIR
 
-            msg_done "Loaded overlays"
-        fi 
-    fi     
+        # For now, presume that /etc/dtbo holds the overlay configs and
+        # we're going to push each and enable, in turn, from that directory.
+        for dtbo in /etc/dtbo/* ; do
+            dtbo_name=`basename $dtbo`
+            mkdir $dtbo_name
+            cp $dtbo $dtbo_name/dtbo
+            echo 1 > $dtbo_name/status
+        done
+
+        msg_done "Loaded overlays"
+    fi
 fi
